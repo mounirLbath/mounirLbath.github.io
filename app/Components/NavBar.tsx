368d9025ca4dcc83865d416761f31597a7cdbc3e
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-03-10 15:14:17 +0100

    Enhance NavBar with mobile menu support and update homepage content

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   currentPath?: string;
@@ -13,26 +14,120 @@ const NavBar = ({ currentPath = "/" }: Props) => {
     { display: "Other", path: "/a" },
   ];
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <div className="sticky top-0 flex items-center justify-between  bg-background/60 backdrop-blur-lg z-10 py-4 px-50">
-      <Link className="font-mono font-black text-2xl" href="/">
-        Mounir Lbath
-      </Link>
-      <div className="space-x-10">
-        {navLinks.map((link, index) => (
-          <Link
-            key={index}
-            href={link.path}
-            className={
-              "text-lg hover:text-blue-500 duration-300 ease-in-out " +
-              (currentPath === link.path && "text-blue-500")
-            }
+    // <nav className="sticky top-0 flex items-center justify-between  bg-background/60 backdrop-blur-lg z-10 py-4 px-50">
+    //   <Link className="font-mono font-black text-2xl" href="/">
+    //     Mounir Lbath
+    //   </Link>
+    //   <div className="sm:items-stretch sm:justify-start">
+    //     <div className="hidden sm:block sm:ml-6">
+    //       <div className="flex space-x-4">
+    //         {navLinks.map((link, index) => (
+    //           <Link
+    //             key={index}
+    //             href={link.path}
+    //             className={
+    //               "text-lg hover:text-blue-500 duration-300 ease-in-out " +
+    //               (currentPath === link.path && "text-blue-500")
+    //             }
+    //           >
+    //             {link.display}
+    //           </Link>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
+
+    <nav className="sticky top-0 flex-col items-center justify-between  bg-background/40 backdrop-blur-lg z-10 py-4">
+      <div className="top-0  flex items-center justify-between mx-10">
+        <div className="font-mono font-black text-2xl">Mounir Lbath</div>
+        <div className="hidden sm:block sm:ml-6">
+          <div className="flex space-x-15">
+            {navLinks.map((link, index) => (
+              <Link
+                key={index}
+                href={link.path}
+                className={
+                  "text-lg hover:text-blue-500 duration-300 ease-in-out " +
+                  (currentPath === link.path && "text-blue-500")
+                }
+              >
+                {link.display}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="sm:hidden">
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex cursor-pointer items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 duration-300 ease-in-out "
+            aria-controls="mobile-menu"
+            aria-expanded={isMenuOpen ? "true" : "false"}
           >
-            {link.display}
-          </Link>
+            <span className="sr-only">Open main menu</span>
+            {/* Mobile menu icon */}
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+            {/* Mobile close icon */}
+            <svg
+              className="hidden h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      {/* Mobile menu, show/hide based on menu state */}
+      <div
+        className={`${
+          isMenuOpen ? "" : "h-0 -translate-x-full"
+        } relative transition-transform  duration-300 ease-in-out  sm:hidden flex-col mx-10`}
+        id="mobile-menu"
+      >
+        {navLinks.map((link, index) => (
+          <div>
+            <Link
+              key={index}
+              href={link.path}
+              className={
+                "text-lg hover:text-blue-500 duration-300 ease-in-out " +
+                (currentPath === link.path && "text-blue-500")
+              }
+            >
+              {link.display}
+            </Link>{" "}
+          </div>
         ))}
       </div>
-    </div>
+    </nav>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,15 @@ export default function Home() {
   return (
     <>
       <NavBar currentPath="/" />
-      <main className="px-50">
-        <h1 className="font-mono text-3xl p-5">About me</h1>
+      <main className="px-[10%]">
+        <h1 className="font-mono text-3xl p-5">Who am I?</h1>
         <p className="font-black text-3xl">
-          My name is Mounir Lbath. I am currently studying Math, Computer
-          Science & Physics at Ecole Polytechnique in Paris.
+          My name is Mounir Lbath. I am a student at Ecole Polytechnique in
+          Paris.
           <br />
         </p>
+        <h1 className="font-mono text-3xl p-5">What do I like?</h1>
+
         <TypeText
           sequence={[
             "I love Math.",
@@ -27,6 +29,49 @@ export default function Home() {
         <Math className="text-2xl flex justify-center">
           {"\\displaystyle{\\frac{\\pi^2}6=\\sum_{i=1}^\\infty \\frac{1}{i^2}}"}
         </Math>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
+        <h1>Hey</h1>
       </main>
     </>
   );
