1e5a070ed6d22450391e6d0d74f5922159532bcd
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-03-13 13:55:38 +0100

    make page a server component once again, shift about me section

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,18 @@
-"use client";
-import { useRef } from "react";
 import TypeText from "./Components/TypeText";
-import Button from "./Components/Button";
 import Title from "./Components/Title";
 import LinkButton from "./Components/LinkButton";
 import DevTools from "./Components/DevTools";
 import SoftwareTools from "./Components/SoftwareTools";
+import ButtonScrollToSection from "./Components/ButtonScrollToSection";
 
 export default function Home() {
-  const sectionRef = useRef<null | HTMLDivElement>(null);
-
-  // Function to scroll to the next section
-  const scrollToSection = () => {
-    if (sectionRef.current) {
-      sectionRef.current?.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
-
   return (
     <>
       <main className="mx-[10%] sm:mx-[20%]">
         <div className="absolute top-15 left-0 rounded-full w-50 h-50 bg-gradient-to-r from-blue-50 to-blue-400 blur-3xl -z-10"></div>
         <div className="absolute top-60 left-70 sm:left-90 lg:left-150 rounded-full w-100 h-80 bg-gradient-to-r from-blue-50/50 to-blue-400/50 blur-xl -z-10"></div>
 
-        <div className="min-h-dvh">
+        <div className="">
           <div className="pt-[35vh] mb-0">
             <p className="text-5xl">Hi, my name is</p>
             <p className="font-mono text-7xl mb-15">Mounir Lbath</p>
@@ -44,11 +30,11 @@ export default function Home() {
               ]}
             />
           </div>
-          <Button onClick={scrollToSection} className="mt-10">
+          <ButtonScrollToSection sectionId="aboutMe" className="mt-10">
             About me ▼
-          </Button>
+          </ButtonScrollToSection>
         </div>
-        <div ref={sectionRef}>
+        <div id="aboutMe" className="pt-10">
           <Title>About me</Title>
           <p>
             I am a first year student at{" "}
