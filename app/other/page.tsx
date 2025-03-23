2be1efa4acfe9741a6c4897f516866776d30c22b
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-03-23 18:28:53 +0100

    update image component to use Next.js Image; allow Google Drive images in config

--- app/other/page.tsx
+++ app/other/page.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const page = () => {
   const id = "1BWXQXbBgGb7wYwjyHV5N2Fii2nvLsidM";
 
   return (
     <div>
-      <img
+      <Image
         src={"https://drive.google.com/thumbnail?sz=w3000&id=" + id}
-        alt=""
-        width="500"
+        alt="Mountains"
+        width="600"
         height="200"
-      ></img>
+        unoptimized
+      />
     </div>
   );
 };
