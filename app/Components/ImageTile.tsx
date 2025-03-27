16e269f170bc3f82521150945944542619ea93b8
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-03-27 19:55:28 +0100

    add photo gallery

---
+++ app/Components/ImageTile.tsx
@@ -0,0 +1,81 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+interface ImageTileProps {
+  index: number;
+  id: string;
+  description?: string;
+  date?: string;
+  properties?: string[];
+  location?: string;
+}
+
+const ImageTile: React.FC<ImageTileProps> = ({
+  index,
+  id,
+  description = "",
+  date = "",
+  properties = [],
+  location = "",
+}) => {
+  const [descriptionOpen, setDescriptionOpen] = useState(false);
+
+  return (
+    <div>
+      <div className="relative overflow-hidden group">
+        <Image
+          src={"https://drive.google.com/thumbnail?sz=w1000&id=" + id}
+          alt={description}
+          quality={100}
+          width={1000}
+          height={100}
+          className="w-full h-auto cursor-pointer transform transition-transform duration-300 ease-in-out group-hover:scale-110"
+          priority={index < 4}
+          onClick={() => {
+            setDescriptionOpen(!descriptionOpen);
+          }}
+        />
+        {
+          <div
+            className={
+              "absolute inset-0 p-5  cursor-pointer bg-background/40 backdrop-blur-lg w-full transition-opacity duration-500 ease-in-out " +
+              (descriptionOpen ? "opacity-100" : "opacity-0")
+            }
+            onClick={() => {
+              setDescriptionOpen(!descriptionOpen);
+            }}
+          >
+            <div className="flex h-full flex-col justify-between">
+              <div>
+                <p className="font-mono text-2xl">{description}</p>
+                {location != null && location != "" ? (
+                  <p className="font-mono text-2xl">{"(" + location + ")"}</p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                {properties != null && properties.length > 0 ? (
+                  <div className="flex gap-5">
+                    {properties.map((v, index) => (
+                      <p key={index} className="">
+                        {v}
+                      </p>
+                    ))}
+                  </div>
+                ) : (
+                  ""
+                )}
+
+                {date != null && date != "" ? <p>{date}</p> : ""}
+              </div>
+            </div>
+          </div>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default ImageTile;
--- app/other/page.tsx
+++ app/other/page.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import Image from "next/image";
+import ImageTile from "../Components/ImageTile";
 
 const page = () => {
   const images = [
     {
       id: "1BWXQXbBgGb7wYwjyHV5N2Fii2nvLsidM",
-      description: "Mountains during sunset",
+      description: "Alps at sunset",
+      location: "Lac d'Anterne, French Alps",
+      date: "August 2024",
+      properties: ["Nikon D3400", "", "f/3.5"],
     },
     {
       id: "1-qZ63doe9Lppr2QgW8oBWjlH2jcsYKwH",
       description: "Milky Way and a tent",
+      location: "Lac d'Anterne, French Alps",
+      date: "August 2024",
+      properties: ["Nikon D3400", "18mm", "f/3.5"],
     },
+
     {
       id: "11bDEUQ6-_9j4ne2ckhD2Iu3nyI9TKt1m",
       description: "Mont blanc",
@@ -19,18 +26,17 @@ const page = () => {
 
   return (
     <>
-      <div className="lg:grid grid-cols-2 gap-5">
+      <div className="grid lg:grid-cols-2 gap-6 gap-y-5">
         {images.map((im, index) => {
           return (
-            <Image
+            <ImageTile
               key={index}
-              src={"https://drive.google.com/thumbnail?sz=w1000&id=" + im.id}
-              alt={im.description}
-              quality={100}
-              width={1000}
-              height={100}
-              className="w-full h-auto"
-              priority={index < 4}
+              index={index}
+              id={im.id}
+              description={im.description}
+              date={im.date}
+              properties={im.properties}
+              location={im.location}
             />
           );
         })}
